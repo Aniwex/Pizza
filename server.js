5c7cdbba312d9ec6c68c01a0a8bc59ee82e7b0dc
Aniwex <rekudo55@gmail.com>
2022-02-22 15:03:00 +0300

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
-const middleware = jsonServer.defaults();
+const middleware = jsonServer.defaults({
+  static: './build',
+});
 
 const PORT = process.env.PORT || 3001;
 
